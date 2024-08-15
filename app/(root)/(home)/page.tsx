a5c71935afe0957048118dfa4ca8e6b3cf05c0e8
Rahmonjon <qosimovrahmonjon0927@gmail.com>
2024-08-15 08:40:57 +0500

    Auth page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,7 +2,7 @@ import Auth from "@/components/auth"
 
 const Page = () => {
   const user = false
-  if(!user) return <Auth/>
+  if(!user) return <div className="container mx-auto max-w-7xl h-screen"><Auth/></div>
   return (
     <div>Page</div>
   )
